fdb3a0d098666639bdbdf370561832e58de4195b
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-05-21 13:35:52 +0100

    Add files for pipeline

---
+++ pipeline/terraform/prerequisites/azure.tf
@@ -0,0 +1,16 @@
+﻿terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.21.0"
+    }
+  }
+
+  required_version = ">=1.2.4"
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ pipeline/terraform/prerequisites/main.tf
@@ -0,0 +1,13 @@
+﻿resource "azurerm_resource_group" "test" {
+  location = "UK South"
+  name     = "srj-test"
+  tags     = {
+    Agg = data.azurerm_app_configuration_key.test.value
+  }
+}
+
+data "azurerm_app_configuration_key" "test" {
+  configuration_store_id = "/subscriptions/9cfe401c-1044-470a-bac0-e1c8e0e85595/resourceGroups/srj-test/providers/Microsoft.AppConfiguration/configurationStores/app-conf-srj"
+  key                    = "terraformKey"
+  label                  = "preq-dev"
+}
